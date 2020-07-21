99cdf6781fec388a9b8323f27391bd0289cc0b2d
Edgar Lepe <lepe.edgar10@gmail.com>
2020-07-20 17:53:35 -0700

    Finshed debugging exercises 2

--- software_engineering_foundations/debugging/debugging_exercise_2/lib/problems.rb
+++ software_engineering_foundations/debugging/debugging_exercise_2/lib/problems.rb
@@ -1,3 +1,57 @@
+# frozen_string_literal: true
+
 # Run `bundle exec rspec` and satisy the specs.
 # You should implement your methods in this file.
 # Feel free to use the debugger when you get stuck.
+
+require 'prime'
+
+def largest_prime_factor(num)
+  factors = []
+
+  (2..num).each { |i| factors << i if (num % i).zero? }
+
+  factors.reduce do |acc, x|
+    if x.prime?
+      x
+    else
+      acc
+    end
+  end
+end
+
+def unique_chars?(str)
+  chars = str.chars
+
+  chars == chars.uniq
+end
+
+def dupe_indices(arr)
+  char_indices = {}
+
+  arr.each.with_index do |x, i|
+    if char_indices[x]
+      char_indices[x] << i
+    else
+      char_indices[x] = [i]
+    end
+  end
+
+  char_indices.keep_if { |_, v| v.length > 1 }
+end
+
+def ana_array(arr1, arr2)
+  ele_counts(arr1) == ele_counts(arr2)
+end
+
+def ele_counts(arr)
+  counts = {}
+  arr.each do |x|
+    if counts[x]
+      counts[x] += 1
+    else
+      counts[x] = 1
+    end
+  end
+  counts
+end
