01bcf613660efdf44cf480b7a0904be7b19c2d5c
Edgar Lepe <lepe.edgar10@gmail.com>
2020-07-23 17:28:03 -0700

    Finished module

---
+++ software_engineering_foundations/Classes/dog_project/lib/dog.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+# Dog class
+class Dog
+  attr_reader :name, :breed, :favorite_foods
+  attr_accessor :age
+
+  def initialize(name, breed, age, bark, favorite_foods)
+    @name = name
+    @breed = breed
+    @age = age
+    @bark = bark
+    @favorite_foods = favorite_foods
+  end
+
+  def bark
+    if @age > 3
+      @bark.upcase
+    else
+      @bark.downcase
+    end
+  end
+
+  def favorite_food?(food)
+    favorite_foods.map(&:downcase).include? food.downcase
+  end
+end
