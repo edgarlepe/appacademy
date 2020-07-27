566774c59329a7cd24339b7178fdb7b9c7de1a32
Edgar Giovanni Lepe <lepe.edgar10@gmail.com>
2020-07-27 14:45:26 -0700

    Finished module

--- software_engineering_foundations/IO/hotel_project/lib/hotel.rb
+++ software_engineering_foundations/IO/hotel_project/lib/hotel.rb
@@ -1,5 +1,42 @@
 require_relative "room"
 
 class Hotel
-  
+  attr_reader :rooms
+
+  def initialize(name, room_capacities)
+    @name = name
+    @rooms = room_capacities.transform_values { |capacity| Room.new capacity }
+  end
+
+  def name
+    words = @name.split ' '
+    words.map!(&:capitalize)
+    words.join ' '
+  end
+
+  def room_exists?(room_name)
+    !@rooms[room_name].nil?
+  end
+
+  def check_in(guest_name, room_name)
+    if room_exists? room_name
+      if @rooms[room_name].add_occupant guest_name
+        puts 'check in successful'
+      else
+        puts 'sorry, room is full'
+      end
+    else
+      puts 'sorry, room does not exist'
+    end
+  end
+
+  def has_vacancy?
+    @rooms.any? { |_, room| !room.full? }
+  end
+
+  def list_rooms
+    @rooms.each do |name, room|
+      puts name + ' ' + room.available_space.to_s
+    end
+  end
 end
--- software_engineering_foundations/IO/hotel_project/lib/room.rb
+++ software_engineering_foundations/IO/hotel_project/lib/room.rb
@@ -1,3 +1,25 @@
 class Room
+  attr_reader :capacity, :occupants
 
+  def initialize(capacity)
+    @capacity = capacity
+    @occupants = []
+  end
+
+  def full?
+    @occupants.length >= @capacity
+  end
+
+  def available_space
+    @capacity - @occupants.length
+  end
+
+  def add_occupant(occupant_name)
+    if full?
+      false
+    else
+      @occupants << occupant_name
+      true
+    end
+  end
 end
