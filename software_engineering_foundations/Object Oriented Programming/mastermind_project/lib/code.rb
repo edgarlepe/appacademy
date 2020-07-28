980efebff652c01bb5065dfa2dbe0dd13ae2f546
Edgar Lepe <lepe.edgar10@gmail.com>
2020-07-27 19:54:10 -0700

    Partially implemented Code class

--- software_engineering_foundations/Object Oriented Programming/mastermind_project/lib/code.rb
+++ software_engineering_foundations/Object Oriented Programming/mastermind_project/lib/code.rb
@@ -1,9 +1,20 @@
 class Code
   POSSIBLE_PEGS = {
-    "R" => :red,
-    "G" => :green,
-    "B" => :blue,
-    "Y" => :yellow
-  }
+    'R' => :red,
+    'G' => :green,
+    'B' => :blue,
+    'Y' => :yellow
+  }.freeze
 
+  attr_reader :pegs
+
+  def self.valid_pegs?(chars)
+    chars.all? { |char| POSSIBLE_PEGS.member? char.upcase }
+  end
+
+  def initialize(chars)
+    raise 'Invalid peg' unless Code.valid_pegs?(chars)
+
+    @pegs = chars.map(&:upcase)
+  end
 end
