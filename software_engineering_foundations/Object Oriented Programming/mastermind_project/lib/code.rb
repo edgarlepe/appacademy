636a65543ac0c6c357be4d15e661c330af20c7b7
Edgar Lepe <lepe.edgar10@gmail.com>
2020-07-28 11:29:48 -0700

    Finished module

--- software_engineering_foundations/Object Oriented Programming/mastermind_project/lib/code.rb
+++ software_engineering_foundations/Object Oriented Programming/mastermind_project/lib/code.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Code
   POSSIBLE_PEGS = {
     'R' => :red,
@@ -16,5 +18,58 @@ class Code
     raise 'Invalid peg' unless Code.valid_pegs?(chars)
 
     @pegs = chars.map(&:upcase)
+  end
+
+  def self.random(len)
+    random_pegs = []
+    len.times { random_pegs << POSSIBLE_PEGS.keys[rand(0..3)] }
+    Code.new random_pegs
+  end
+
+  def self.from_string(pegs_string)
+    Code.new pegs_string.chars
+  end
+
+  def [](index)
+    @pegs[index]
+  end
+
+  def length
+    @pegs.length
+  end
+
+  def num_exact_matches(guess)
+    min_length = guess.length < length ? guess.length : length
+    count = 0
+    (0...min_length).each { |i| count += 1 if guess[i] == @pegs[i] }
+    count
+  end
+
+  def num_near_matches(guess)
+    count = 0
+    POSSIBLE_PEGS.keys.each do |key|
+      self_count = @pegs.count(key)
+      guess_count = guess.pegs.count(key)
+      exact_matches_for_key = num_exact_matches_for_key(key, guess)
+      count += [self_count, guess_count - exact_matches_for_key].min
+    end
+    count
+  end
+
+  def ==(other)
+    false if length != other.length
+
+    pegs == other.pegs
+  end
+
+  private
+
+  def num_exact_matches_for_key(key, guess)
+    min_length = guess.length < length ? guess.length : length
+    count = 0
+    (0...min_length).each do |i|
+      count += 1 if @pegs[i] == key && guess[i] == @pegs[i]
+    end
+    count
   end
 end
--- software_engineering_foundations/Object Oriented Programming/mastermind_project/lib/mastermind.rb
+++ software_engineering_foundations/Object Oriented Programming/mastermind_project/lib/mastermind.rb
@@ -1,5 +1,19 @@
 require_relative "code"
 
 class Mastermind
+  def initialize(length)
+    @secret_code = Code.random length
+  end
 
+  def print_matches(code)
+    puts "Exact matches: #{@secret_code.num_exact_matches code}"
+    puts "Near matches: #{@secret_code.num_near_matches code}"
+  end
+
+  def ask_user_for_guess
+    print 'Enter a code: '
+    user_code = Code.from_string gets.chomp
+    print_matches(user_code)
+    @secret_code == user_code
+  end
 end
