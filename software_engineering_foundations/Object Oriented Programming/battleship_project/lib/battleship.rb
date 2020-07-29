50b34bc85e3a6f89d7c05ed972c0855177b207fb
Edgar Lepe <lepe.edgar10@gmail.com>
2020-07-29 00:15:04 -0700

    Finished module

--- software_engineering_foundations/Object Oriented Programming/battleship_project/lib/battleship.rb
+++ software_engineering_foundations/Object Oriented Programming/battleship_project/lib/battleship.rb
@@ -2,5 +2,43 @@ require_relative "board"
 require_relative "player"
 
 class Battleship
+  attr_reader :board, :player
 
+  def initialize(n)
+    @player = Player.new
+    @board = Board.new n
+    @remaining_misses = @board.size / 2
+  end
+
+  def start_game
+    @board.place_random_ships
+    puts "#{@board.size / 4} ships placed on board"
+    @board.print
+  end
+
+  def lose?
+    return false if @remaining_misses.positive?
+
+    puts 'you lose'
+    true
+  end
+
+  def win?
+    return false if @board.num_ships.positive?
+
+    puts 'you win'
+    true
+  end
+
+  def game_over?
+    win? || lose?
+  end
+
+  def turn
+    position = @player.get_move
+    successful_attack = @board.attack position
+    @board.print
+    @remaining_misses -= 1 unless successful_attack
+    puts "#{@remaining_misses} misses remain"
+  end
 end
--- software_engineering_foundations/Object Oriented Programming/battleship_project/lib/player.rb
+++ software_engineering_foundations/Object Oriented Programming/battleship_project/lib/player.rb
@@ -1,3 +1,7 @@
 class Player
-
+  def get_move
+    puts 'enter a position with coordinates seperated with a space like `4 7`: '
+    user_input = gets.chomp
+    user_input.split.map(&:to_i)
+  end
 end
