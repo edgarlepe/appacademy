1cf48d912501b8deae086c6d92906dd66b7c205e
Edgar Giovanni Lepe <lepe.edgar10@gmail.com>
2020-07-28 22:37:38 -0700

    Finished part 2

--- software_engineering_foundations/Object Oriented Programming/battleship_project/lib/board.rb
+++ software_engineering_foundations/Object Oriented Programming/battleship_project/lib/board.rb
@@ -1,3 +1,65 @@
+# frozen_string_literal: true
+
+# Board model
 class Board
-  
+  attr_reader :size
+
+  def initialize(n)
+    @grid = Array.new(n) { Array.new n, :N }
+    @size = n**2
+  end
+
+  def [](indices)
+    @grid[indices[0]][indices[1]]
+  end
+
+  def []=(position, value)
+    @grid[position[0]][position[1]] = value
+  end
+
+  def num_ships
+    @grid.flatten.count :S
+  end
+
+  def attack(position)
+    if self[position] == :S
+      self[position] = :H
+      puts 'you sunk my battleship!'
+      true
+    else
+      self[position] = :X
+      false
+    end
+  end
+
+  def place_random_ships
+    dim = Integer.sqrt(size)
+    quarter = size / 4
+    positions = []
+
+    until positions.length == quarter && positions.uniq!.nil?
+      positions << rand(0...size)
+    end
+
+    positions.each do |pos|
+      position = [(pos / dim), (pos % dim)]
+      self[position] = :S
+    end
+  end
+
+  def hidden_ships_grid
+    @grid.map { |row| row.map { |val| val == :S ? :N : val } }
+  end
+
+  def self.print_grid(grid)
+    grid.each { |row| puts row.join(' ') }
+  end
+
+  def cheat
+    Board.print_grid @grid
+  end
+
+  def print
+    Board.print_grid hidden_ships_grid
+  end
 end
