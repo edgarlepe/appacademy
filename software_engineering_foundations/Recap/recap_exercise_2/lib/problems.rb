f7b5f67d9b8d8a13a54e91e0a21d17fab1dfe569
Edgar Lepe <lepe.edgar10@gmail.com>
2020-07-30 22:59:04 -0700

    Finished module

--- software_engineering_foundations/Recap/recap_exercise_2/lib/problems.rb
+++ software_engineering_foundations/Recap/recap_exercise_2/lib/problems.rb
@@ -1,21 +1,73 @@
 # frozen_string_literal: true
 
+require 'prime'
+
+class String
+  def bigrams
+    words = split
+    arr = []
+    words.each do |word|
+      (0..1).each do |i|
+        arr += __take_2(word[i..].length / 2, word[i..])
+      end
+    end
+    arr.uniq
+  end
+
+  private
+
+  def __take_2(n, str)
+    return [] if n.zero?
+
+    [str.chars.take(2).join] + __take_2(n - 1, str.chars.drop(2).join)
+  end
+end
+
 # Write a method, least_common_multiple, that takes in two numbers and returns the smallest number that is a mutiple
 # of both of the given numbers
-def least_common_multiple(num_1, num_2); end
+def least_common_multiple(num1, num2)
+  prime_factorization = lcm_prime_factorization(num1, num2)
+  prime_factorization.reduce(1) { |acc, pair| acc * (pair[0]**pair[1]) }
+end
+
+def lcm_prime_factorization(num1, num2)
+  prime_factorization_union = Prime.prime_division(num1).union(Prime.prime_division(num2)).sort!
+  prime_factorization_union.reject! do |pair|
+    same_prime_index = prime_factorization_union.rindex { |x| x[0] == pair[0] }
+    same_prime_index && prime_factorization_union[same_prime_index][1] > pair[1]
+  end
+  prime_factorization_union
+end
 
 # Write a method, most_frequent_bigram, that takes in a string and returns the two adjacent letters that appear the
 # most in the string.
-def most_frequent_bigram(str); end
+def most_frequent_bigram(str)
+  bigrams = str.bigrams
+  most_freq_with_count = bigrams.reduce(['', 0]) do |acc, bigram|
+    count = str.scan(/#{bigram}/).length
+    acc[1] > count ? acc : [bigram, count]
+  end
+  most_freq_with_count[0]
+end
 
 class Hash
   # Write a method, Hash#inverse, that returns a new hash where the key-value pairs are swapped
-  def inverse; end
+  def inverse
+    new_hash = {}
+    each_pair { |k, v| new_hash[v] = k }
+    new_hash
+  end
 end
 
 class Array
   # Write a method, Array#pair_sum_count, that takes in a target number returns the number of pairs of elements that sum to the given target
-  def pair_sum_count(num); end
+  def pair_sum_count(num)
+    count = 0
+    (0...length - 1).each do |i|
+      self[i + 1..].each { |x| count += 1 if self[i] + x == num }
+    end
+    count
+  end
 
   # Write a method, Array#bubble_sort, that takes in an optional proc argument.
   # When given a proc, the method should sort the array according to the proc.
@@ -30,5 +82,8 @@ class Array
   # it does not matter which element goes first (i.e. do nothing).
   #
   # This should remind you of the spaceship operator! Convenient :)
-  def bubble_sort(&prc); end
+  def bubble_sort(&prc)
+    prc ||= proc { |a, b| a <=> b }
+    sort!(&prc)
+  end
 end
